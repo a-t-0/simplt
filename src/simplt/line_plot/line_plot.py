e59232473ea4c513d58266e80a7d8d786e014e00
name <you@example.com>
2023-08-05 15:18:36 +0200

    Included colours for lines.

--- src/simplt/line_plot/line_plot.py
+++ src/simplt/line_plot/line_plot.py
@@ -5,7 +5,8 @@ For copy-pastable examples, see:     example_create_a_table()
 example_create_multi_line_plot()     example_create_single_line_plot()
 at the bottom of this file.
 """
-from typing import Any, List, Optional
+from ctypes import Union
+from typing import Any, List, Optional, Tuple
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -13,7 +14,60 @@ import numpy as np
 from matplotlib import lines
 from typeguard import typechecked
 
+# pylint: disable=R0902
+# pylint: disable=R0903
+class Line:
+    """Stores a line that can be plotted.
+    """
 
+    # pylint: disable=R0913
+    def __init__(
+        self,
+        x_series: List[float],
+        y_series: List[float],
+        label:str,
+        colour: Optional[str] = None,
+    ):
+        self.x_series: List[float]=x_series
+        self.y_series: List[float]=y_series
+        if len(self.x_series) !=len(self.y_series):
+            raise ValueError("Error, x and y series should be equal nr of nrs.")
+        
+        self.label:str=label
+        self.colour: Union[None,str]= colour
+
+
+
+class X_tick:
+    """Stores a line that can be plotted.
+    """
+
+    # pylint: disable=R0913
+    def __init__(
+        self,
+        x_pos:float,
+        x_pos_label:str,
+    ):
+        self.x_pos: float=x_pos
+        self.x_pos_label:str=x_pos_label
+
+class Window_lim:
+    """Stores a line that can be plotted.
+    """
+
+    # pylint: disable=R0913
+    def __init__(
+        self,
+        x_min:float,
+        x_max:float,
+        y_min:float,
+        y_max:float,
+    ):
+        self.x_min:float=x_min
+        self.x_max:float=x_max
+        self.y_min:float=y_min
+        self.y_max:float=y_max
+        
 @typechecked
 def example_create_multi_line_plot(
     output_dir: str, filename: str, extensions: List[str]
@@ -50,13 +104,14 @@ def example_create_multi_line_plot(
 def plot_multiple_lines(
     extensions: List[str],
     filename: str,
-    label: List,
     legendPosition: int,
     output_dir: str,
-    x: List,
+    lines: List[Line],
     x_axis_label: str,
     y_axis_label: str,
-    y_series: np.ndarray,
+    window_lim:Window_lim,
+    title:Optional[str],
+    x_ticks:Optional[List[Tuple[float,float]]],
 ) -> None:
     """
 
@@ -74,26 +129,34 @@ def plot_multiple_lines(
     # TODO: reduce 9/5 arguments to at most 5/5 arguments.
     fig = plt.figure()
     ax = fig.add_subplot(111)
+    if title is not None:
+        ax.set_title(title)
 
     # Set line colours in plot object.
-    set_cmap(some_plt=plt, nr_of_colours=len(y_series[:, 0]), name="hsv")
+    set_cmap(some_plt=plt, nr_of_colours=len(lines), name="hsv")
 
     # Generate line types.
-    lineTypes = generateLineTypes(y_series)
+    lineTypes = generateLineTypes(list(map(lambda line:line.y_series,lines)))
 
     # Geneterate lines.
-    for i in range(0, len(y_series)):
+    for i in range(0, len(lines)):
         ax.plot(
-            x,
-            y_series[i, :],
+            lines[i].x_series,
+            lines[i].y_series,
             ls=lineTypes[i],
-            label=label[i],
+            label=lines[i].label,
             fillstyle="none",
         )
+
+    plt.xlim(window_lim.x_min,window_lim.x_max)
+    plt.ylim(window_lim.y_min,window_lim.y_max)
 
     # configure plot layout
     plt.legend(loc=legendPosition)
     plt.xlabel(x_axis_label)
+    # You can specify a rotation for the tick labels in degrees or with keywords.
+    # 0=x position of x-tick, 1=text of xtick.
+    plt.xticks(list(map(lambda x_tick:x_tick.x_pos,x_ticks)), list(map(lambda x_tick:x_tick.x_pos_label,x_ticks)), rotation='horizontal')
     plt.ylabel(y_axis_label)
     for extension in extensions:
         plt.savefig(f"{output_dir}/{filename}{extension}")
@@ -122,7 +185,7 @@ def set_cmap(
 
 
 @typechecked
-def generateLineTypes(y_series: np.ndarray) -> List:
+def generateLineTypes(y_series: List) -> List:
     """
 
     :param y_series:
